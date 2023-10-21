3c38a18fd9d959e3aca9aa31e71aac0d58021d47
jake9190 <jake9190@hotmail.com>
2023-10-21 00:38:12 -0400

    Update ring-virtual-light-with-siren.groovy

--- src/drivers/ring-virtual-light-with-siren.groovy
+++ src/drivers/ring-virtual-light-with-siren.groovy
@@ -27,7 +27,7 @@ metadata {
     attribute "firmware", "string"
     attribute "rssi", "number"
     attribute "wifi", "string"
-    attribute "connection", "string"
+    attribute 'healthStatus', 'enum', [ 'unknown', 'offline', 'online' ]
 
     command "alarmOff"
     command "getDings"
@@ -238,7 +238,7 @@ void handleMotion(final Map msg) {
 
 void handleRefresh(final Map msg) {
   if (msg?.alerts?.connection != null) {
-    checkChanged("connection", msg.alerts.connection) // devices seem to be considered offline after 20 minutes
+    checkChanged("healthStatus", msg.alerts.connection) // devices seem to be considered offline after 20 minutes
   }
   else {
     log.warn("No connection information found in response")
