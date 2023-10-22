bded4f967803fdaee48f58a95638572da3e77d76
jake9190 <jake9190@hotmail.com>
2023-10-21 22:46:03 -0400

    Update ring-virtual-light-with-siren.groovy

--- src/drivers/ring-virtual-light-with-siren.groovy
+++ src/drivers/ring-virtual-light-with-siren.groovy
@@ -23,16 +23,19 @@ metadata {
     capability "Refresh"
     capability "Sensor"
     capability "Switch"
-    capability 'Health Check'
+    capability "Health Check"
 
     attribute "firmware", "string"
     attribute "rssi", "number"
     attribute "wifi", "string"
-    
-    attribute 'healthStatus', 'enum', [ 'unknown', 'offline', 'online' ]
+    attribute "healthStatus", "enum", [ "unknown", "offline", "online" ]
 
     command "alarmOff"
     command "getDings"
+    command "snoozeMotionAlerts", [
+        [name:"minutes", type:"NUMBER", description:"Number of minutes to snooze motion alerts for", constraints:["NUMBER"]] ]
+    command "setMotionActivatedLights", [
+        [name:"state", description:"Enable or disable lights on motion", type: "ENUM", constraints: ["enabled","disabled"]] ]
   }
 
   preferences {
@@ -72,20 +75,6 @@ def refresh() {
   logDebug "refresh()"
   parent.apiRequestDeviceRefresh(device.deviceNetworkId)
   parent.apiRequestDeviceHealth(device.deviceNetworkId, "doorbots")
-  scheduleDevicePolling()
-}
-
-def pollDeviceStatus() {
-  logTrace "pollDeviceStatus()"
-  refresh()
-}
-
-def scheduleDevicePolling() {
-  unschedule(pollDeviceStatus)
-  if (deviceStatusPollingEnable) {
-      Random rnd = new Random()
-      schedule( "${rnd.nextInt(59)} ${rnd.nextInt(9)}/10 * ? * *", "pollDeviceStatus" )
-  }
 }
 
 def getDings() {
@@ -108,10 +97,34 @@ def pollLight() {
   }
 }
 
+def snoozeMotionAlerts(minutes = 60) {
+    parent.apiRequestDeviceControl(device.deviceNetworkId, "doorbots", "motion_snooze?time=${minutes}", null)
+}
+
+def setMotionActivatedLights(state) {
+    // This is backwards as it's manipulating "always snooze"
+    stateOfLight = state == "enabled" ? false : true
+    parent.apiRequestDeviceApiSet(device.deviceNetworkId, "devices", "settings", ["enable":stateOfLight,"light_snooze_settings":["always_on":stateOfLight]])
+}
+
 def updated() {
   setupPolling()
   parent.snapshotOption(device.deviceNetworkId, snapshotPolling)
   scheduleDevicePolling()
+}
+
+def installed() {
+  scheduleDevicePolling()
+}
+
+def scheduleDevicePolling() {
+  unschedule(pollDeviceStatus)
+  // Schedule at a random second starting at the next minute
+  def nextMinute = ((new Date().format( "m" ) as int) + 1) % 60
+  Random rnd = new Random()
+  if (deviceStatusPollingEnable) {
+      schedule( "${rnd.nextInt(59)} ${nextMinute}/30 * ? * *", "refresh" )
+  }
 }
 
 def on() {
@@ -192,6 +205,9 @@ void handleDeviceSet(final String action, final Map msg, final Map query) {
   else if (action == "siren_off") {
     checkChanged('alarm', "off")
   }
+  else if (action == "settings") {
+    logTrace("Updated setting: ${query}")
+  }
   else {
     log.error "handleDeviceSet unsupported action ${action}, msg=${msg}, query=${query}"
   }
@@ -221,10 +237,13 @@ void handleMotion(final Map msg) {
 }
 
 void handleRefresh(final Map msg) {
-  if (msg.alerts?.connection != null) {
+  if (msg?.alerts?.connection != null) {
     checkChanged("healthStatus", msg.alerts.connection) // devices seem to be considered offline after 20 minutes
   }
-  
+  else {
+    checkChanged("healthStatus", "unknown")
+  }
+    
   if (msg.led_status) {
     checkChanged("switch", msg.led_status)
   }
@@ -235,6 +254,10 @@ void handleRefresh(final Map msg) {
     if (secondsRemaining > 0) {
       runIn(secondsRemaining + 1, refresh)
     }
+  }
+
+  if (msg.is_sidewalk_gateway) {
+    log.warn("Your device is being used as an Amazon sidewalk device.")
   }
 
   if (msg.health) {
